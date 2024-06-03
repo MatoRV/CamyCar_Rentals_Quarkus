3e78e17bf1688c950069a895d2ed9a8481478523
MatoRV <victormator5@gmail.com>
2024-06-03 12:27:02 +0200

    Creado endpoint para devolver una lista con las localidades disponibles

---
+++ src/main/java/camycar_rentals/controller/LocalidadController.java
@@ -0,0 +1,35 @@
+package camycar_rentals.controller;
+
+import java.util.List;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import base.controller.BaseController;
+import camycar_rentals.service.LocalidadService;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/localidades")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@RequestScoped
+public class LocalidadController extends BaseController {
+
+    @Inject
+    LocalidadService localidadService;
+
+    @GET
+    @Operation(summary = "Muestra una lista de las localidades")
+    @APIResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = List.class)))
+    @APIResponse(responseCode = "404", description = "NOT FOUND")
+    public Response obtenerLocalidades() {
+        return Response.status(Response.Status.OK).entity(localidadService.obtenerLocalidades()).build();
+    }
+}
--- src/main/java/camycar_rentals/service/LocalidadService.java
+++ src/main/java/camycar_rentals/service/LocalidadService.java
@@ -1,12 +1,27 @@
 package camycar_rentals.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import base.service.BaseService;
 import camycar_rentals.domain.Localidad;
+import camycar_rentals.dto.converters.ConverterJpaToDto;
 import camycar_rentals.repository.LocalidadRepository;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 
 @RequestScoped
-public class LocalidadService extends BaseService<LocalidadRepository, Localidad,Integer> {
+public class LocalidadService extends BaseService<LocalidadRepository, Localidad, Integer> {
 
+    @Inject
+    ConverterJpaToDto converterJpaToDto;
+
+    public List<String> obtenerLocalidades() {
+        List<Localidad> localidades = findAll();
+        List<String> localidadesList = new ArrayList<>();
+        for (Localidad localidad : localidades) {
+            localidadesList.add(localidad.getNombre());
+        }
+        return localidadesList;
+    }
 
 }
