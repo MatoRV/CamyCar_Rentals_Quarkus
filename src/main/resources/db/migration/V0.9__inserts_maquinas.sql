c4c393e49c907fd224426cb7b79063b1b48b97e6
VMatoR <victormator5@gmail.com>
2024-06-18 00:42:08 +0200

    inserta maquina

---
+++ src/main/resources/db/migration/V0.9__inserts_maquinas.sql
@@ -0,0 +1,82 @@
+INSERT INTO tipo_maquina (id_tipo_maquina, nombre) VALUES (1, "Elevadora");
+INSERT INTO tipo_maquina (id_tipo_maquina, nombre) VALUES (2, "Torito");
+INSERT INTO tipo_maquina (id_tipo_maquina, nombre) VALUES (3, "Carretilla");
+
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (1, "Caterpillar", "X200", 1500, "D", 1, 4000);
+-- Insert 1
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (2, "Caterpillar", "X250", 2000, "D", 1, 4200);
+
+-- Insert 2
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (3, "Toyota", "Yale-3000", 2500, "D", 2, 3800);
+
+-- Insert 3
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (4, "Liebherr", "LTM 11200-9.1", 12000, "D", 3, 96000);
+
+-- Insert 4
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (5, "Komatsu", "PC360LC-11", 36000, "D", 1, 35900);
+
+-- Insert 5
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (6, "Hyster", "S120FT PRS", 6000, "D", 2, 9000);
+
+-- Insert 6
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (7, "Kobelco", "BM700", 700, "D", 3, 2100);
+
+-- Insert 7
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (8, "Volvo", "EC480DL", 48000, "D", 1, 48500);
+
+-- Insert 8
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (9, "Clark", "C75D", 7500, "D", 2, 10000);
+
+-- Insert 9
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (10, "Terex", "AC 1000", 10000, "D", 3, 35000);
+
+-- Insert 10
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (11, "Doosan", "DX140LC-5", 14000, "D", 1, 15300);
+
+-- Insert 11
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (12, "Yale", "GLC040SVX", 4000, "D", 2, 5600);
+
+-- Insert 12
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (13, "Liebherr", "LR 1600/2-W", 600000, "D", 3, 1870000);
+
+-- Insert 13
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (14, "JCB", "220X", 20000, "D", 1, 21300);
+
+-- Insert 14
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (15, "Hyster", "H280HD2", 28000, "D", 2, 42000);
+
+-- Insert 15
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (16, "Tadano", "GR-1000XL", 80000, "D", 3, 52600);
+
+-- Insert 16
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (17, "Caterpillar", "335F L CR", 38000, "D", 1, 35000);
+
+-- Insert 17
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (18, "Toyota", "7FGU35", 3500, "D", 2, 4500);
+
+-- Insert 18
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (19, "Liebherr", "LR 11350", 1350000, "D", 3, 2750000);
+
+-- Insert 19
+INSERT INTO maquina (id_maquina, fabricante, modelo, capacidad_carga, estado, id_tipo_maquina, peso)
+VALUES (20, "Komatsu", "PC700LC-11", 70000, "D", 1, 67600);
+
